b08f6a0612abb2ef638f0b9486cf58955504373a
PACIFICABS\urvish.kadia <urvish.kadia@technomark.io>
2024-06-27 12:01:12 +0530

    API Integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,10 +58,10 @@ $(document).ready(function () {
     $(".main-header").load("header.html", function () {
         $(".dropdown").hover(
             function () {
-                $(this).find(".dropdown-menu").stop(true, true).delay(100).fadeIn(200);
+                $(this).find(".dropdown-menu").stop(true, true).delay(100).slideDown(800);
             },
             function () {
-                $(this).find(".dropdown-menu").stop(true, true).delay(100).fadeOut(200);
+                $(this).find(".dropdown-menu").stop(true, true).delay(100).slideUp(800);
             }
         );
     });
@@ -324,3 +324,200 @@ $(window).scroll(function () {
 closeBtn.click(function() {
     btn.hide();
 });
+
+
+        document.getElementById('mainContactForm').addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent default form submission
+    
+            // Perform custom validation
+            let isValid = true;
+    
+            const fullName1 = document.getElementById('fullName1');
+            const email1 = document.getElementById('email1');
+            const phone1 = document.getElementById('phone1');
+            const CompanyName1 = document.getElementById('CompanyName1');
+    
+            const fullNameError1 = document.getElementById('fullNameError1');
+            const emailError1 = document.getElementById('emailError1');
+            const phoneError1 = document.getElementById('phoneError1');
+            const CompanyError1 = document.getElementById('CompanyError1');
+            const message1 = document.getElementById('message1');
+            const erroemessage1 = document.getElementById('erroemessage1');
+    
+            // Clear previous error messages
+            fullNameError1.style.display = 'none';
+            emailError1.style.display = 'none';
+            phoneError1.style.display = 'none';
+            CompanyError1.style.display = 'none';
+            message1.style.display = 'none';
+            erroemessage1.style.display = 'none';
+    
+            if (!fullName1.value.trim()) {
+                fullNameError1.style.display = 'block';
+                isValid = false;
+            }
+    
+            if(!email1.validity.valid || !validateEmail(email1.value)) {
+                emailError1.style.display = 'block';
+                isValid = false;
+            }
+    
+            if (!phone1.validity.valid || phone1.value.length !== 10) {
+                phoneError1.style.display = 'block';
+                isValid = false;
+            }
+    
+            if (!CompanyName1.value.trim()) {
+                CompanyError1.style.display = 'block';
+                isValid = false;
+            }
+    
+    
+            if (isValid) {
+                // Capture form data
+                const formData = {
+                    formType: document.querySelector('input[name="formType"]').value,
+                    fullName: fullName1.value,
+                    email: email1.value,
+                    phone: phone1.value,
+                    CompanyName: CompanyName1.value,
+                    projectDescription: null
+                };
+    
+                // Log the form data to console
+                console.log('Form Data:', formData);
+    
+                // Send form data to server using fetch
+                fetch('https://crm-stageapi.pacificabs.com:3003/api/form', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formData)
+                })
+                // .then(response => response.json())
+                .then(response => {
+                    if(response.ResponseStatus == "success"){
+                        message1.style.display = 'block';
+                        document.getElementById('mainContactForm').reset();
+                    }
+                    else{
+                        erroemessage1.style.display = 'block';
+                    }
+                    // console.log('Success:', data);
+                    
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            }
+        });
+        function validateEmail(email) {
+                const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return re.test(email);
+            }
+
+
+
+    
+    document.getElementById('projectForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Perform custom validation
+        let isValid = true;
+
+        const fullName = document.getElementById('fullName');
+        const email = document.getElementById('email');
+        const phone = document.getElementById('phone');
+        const organizationName = document.getElementById('organizationName');
+        const projectDescription = document.getElementById('projectDescription');
+
+        const fullNameError = document.getElementById('fullNameError');
+        const emailError = document.getElementById('emailError');
+        const phoneError = document.getElementById('phoneError');
+        const organizationNameError = document.getElementById('organizationNameError');
+        const projectDescriptionError = document.getElementById('projectDescriptionError');
+        const message = document.getElementById('message');
+        const erroemessage = document.getElementById('erroemessage');
+
+        // Clear previous error messages
+        fullNameError.style.display = 'none';
+        emailError.style.display = 'none';
+        phoneError.style.display = 'none';
+        organizationNameError.style.display = 'none';
+        projectDescriptionError.style.display = 'none';
+        message.style.display = 'none';
+        erroemessage.style.display = 'none';
+
+        if (!fullName.value.trim()) {
+            fullNameError.style.display = 'block';
+            isValid = false;
+        }
+
+        if(!email.validity.valid || !validateEmail(email.value)) {
+            emailError.style.display = 'block';
+            isValid = false;
+        }
+
+        if (!phone.validity.valid || phone.value.length !== 10) {
+            phoneError.style.display = 'block';
+            isValid = false;
+        }
+
+        if (!organizationName.value.trim()) {
+            organizationNameError.style.display = 'block';
+            isValid = false;
+        }
+
+        if (!projectDescription.value.trim()) {
+            projectDescriptionError.style.display = 'block';
+            isValid = false;
+        }
+
+        if (isValid) {
+            // Capture form data
+            const formData = {
+                formType: document.querySelector('input[name="formType"]').value,
+                fullName: fullName.value,
+                email: email.value,
+                phone: phone.value,
+                organizationName: organizationName.value,
+                projectDescription: projectDescription.value
+            };
+
+            // Log the form data to console
+            console.log('Form Data:', formData);
+
+            // Send form data to server using fetch
+            fetch('https://crm-stageapi.pacificabs.com:3003/api/form', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            // .then(response => response.json())
+            .then(response => {
+                if(response.ResponseStatus == "success"){
+                    message.style.display = 'block';
+                    document.getElementById('projectForm').reset();
+                }
+                else{
+                    erroemessage.style.display = 'block';
+                }
+                // console.log('Success:', data);
+                
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+    });
+    function validateEmail(email) {
+            const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return re.test(email);
+        }
+
+        
+
+   
